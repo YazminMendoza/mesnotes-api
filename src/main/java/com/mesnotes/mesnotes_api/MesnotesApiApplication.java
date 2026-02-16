02748617c10a47ab98de92e0e0e7ebf8caccae29
YazminMendoza <medyinmv@gmail.com>
2026-02-16 12:12:15 +0100

    Mise en place initiale du projet Spring

---
+++ src/main/java/com/mesnotes/mesnotes_api/MesnotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.mesnotes.mesnotes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesnotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesnotesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mesnotes/mesnotes_api/MesnotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesnotes.mesnotes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesnotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
